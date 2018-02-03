9f546f6c2544d563a78452b2b7e25a8a532a653a
Sattar Hummatli <settarxan@gmail.com>
2018-02-03 09:01:52 +0400

    Try varliables and if, switch statements

--- mud/web/main.dart
+++ mud/web/main.dart
@@ -7,5 +7,29 @@ void main() {
 clickedOnEnter(e) {
   InputElement input = querySelector("#commands");
   input.style.borderColor = "#55FF55";
-  querySelector("#output").appendHtml("<div>${input.value}</div>");
+  String output = "";
+
+  switch (input.value) {
+  case 'left':
+    //updateCoordinates(-1, 0);
+    output = "You are going left now.";
+    break;
+  case 'right':
+    //updateCoordinates(1, 0);
+    output = "You are going right, are you sure?";
+    break;
+  case 'up':
+    //updateCoordinates(0, -1);
+    output = "You are going up, are you sure?";
+    break;
+  case 'down':
+    //updateCoordinates(0, 1);
+    output = "You are going down, are you sure?";
+    break;
+  default:
+    output = "Use left, right, up, down please ...";
+  }
+
+  var outputHTML = "<div>${output}</div>";
+  querySelector("#output").appendHtml(outputHTML);
 }
