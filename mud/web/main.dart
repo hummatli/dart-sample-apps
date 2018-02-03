b31c1be1d8f06f65dfabe59670e821299bbfb1a4
Sattar Hummatli <settarxan@gmail.com>
2018-02-03 08:51:51 +0400

    change HTML styles programatically

--- mud/web/main.dart
+++ mud/web/main.dart
@@ -1,11 +1,11 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running. My Changes1';
   querySelector('#enter').onClick.listen(clickedOnEnter);
 }
 
 clickedOnEnter(e) {
   InputElement input = querySelector("#commands");
-  querySelector("#output").appendHtml(input.value);
+  input.style.borderColor = "#55FF55";
+  querySelector("#output").appendHtml("<div>${input.value}</div>");
 }
