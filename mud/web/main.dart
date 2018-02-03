6910459d41790d5a4aceed24a21cda3765285ee6
Sattar Hummatli <settarxan@gmail.com>
2018-02-03 08:44:12 +0400

    add some DOM manipulation functionality

--- mud/web/main.dart
+++ mud/web/main.dart
@@ -1,5 +1,11 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running. My Changes';
+  querySelector('#output').text = 'Your Dart app is running. My Changes1';
+  querySelector('#enter').onClick.listen(clickedOnEnter);
+}
+
+clickedOnEnter(e) {
+  InputElement input = querySelector("#commands");
+  querySelector("#output").appendHtml(input.value);
 }
