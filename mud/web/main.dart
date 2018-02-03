73ca3605ab6e3ef5093cdbae3dd1b5ea81408657
Sattar Hummatli <settarxan@gmail.com>
2018-02-03 10:05:07 +0400

    try getter and setter methods of class

--- mud/web/main.dart
+++ mud/web/main.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 import 'dart:html';
+
 import 'lib/mud.dart';
 
 int x = 0, y = 0;
@@ -11,7 +12,7 @@ void main() {
   updateCoordinates(0,0);
 }
 
-Environment myEnvironment = new Environment("wood");
+Environment myEnvironment = new Environment("Wood");
 
 void clickedOnEnter(e) {
   InputElement input = querySelector("#commands");
@@ -43,12 +44,11 @@ void clickedOnEnter(e) {
 
   querySelector('#output').appendHtml(outputHTML);
 
-  input.style.borderColor = "#55FF55";
-
-
   var interaction = myEnvironment.stumbleUpon();
-  outputHTML = "<div>${interaction}</div>";
-  querySelector("#output").appendHtml(outputHTML);
+  outputHTML= "<div>${interaction}</div>";
+  querySelector('#output').appendHtml(outputHTML);
+
+  input.style.borderColor = "#55FF55";
 }
 
 void updateCoordinates(int rel_x, int rel_y) {
@@ -56,31 +56,4 @@ void updateCoordinates(int rel_x, int rel_y) {
   y += rel_y;
 
   querySelector("#coordinates").innerHtml = "Place : $x , $y";
-}
-
-class Environment {
-  String name;
-
-  Encounter encounter = new Bear();
-
-  Environment(this.name);
-
-  String stumbleUpon() {
-    return "${this.name} you stumbled upon something ${this.encounter.whenEncounter()}";
-  }
-}
-
-
-class Bear extends Encounter {
-
-  whenEncounter() {
-    return "grr grow, you encounter a bear";
-  }
-}
-
-class Ghost extends Encounter {
-
-  whenEncounter() {
-    return "Booh, you encounter a ghost";
-  }
 }
